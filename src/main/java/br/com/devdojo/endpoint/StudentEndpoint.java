55e6e59d156b5912feed0409a59b523955789ea4
Renan Rezenes Magalhaes Vieira <renanrezenes@brq.com>
2019-04-23 17:57:46 -0300

    Initial Commit

---
+++ src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
@@ -0,0 +1,53 @@
+package br.com.devdojo.endpoint;
+
+import br.com.devdojo.error.CustomErrorType;
+import br.com.devdojo.model.Student;
+import br.com.devdojo.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("students")
+public class StudentEndpoint {
+
+    @Autowired
+    private DateUtil dateUtil;
+
+    @GetMapping
+    public ResponseEntity<?> listAll() {
+        return new ResponseEntity<>(Student.studentList, HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
+        Student student = new Student();
+        student.setId(id);
+        int index = Student.studentList.indexOf(student);
+        if(index == -1)
+            return new ResponseEntity<>(new CustomErrorType("Student not found"),HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody Student student) {
+        Student.studentList.add(student);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> delete(@RequestBody Student student) {
+        Student.studentList.remove(student);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody Student student) {
+        Student.studentList.remove(student);
+        Student.studentList.add(student);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/br/com/devdojo/model/Student.java
@@ -0,0 +1,63 @@
+package br.com.devdojo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.Arrays.asList;
+
+public class Student {
+    private int id;
+    private String name;
+    public static List<Student> studentList;
+
+    static {
+        studentRepository();
+    }
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student() {
+    }
+
+    private static void studentRepository(){
+        studentList = new ArrayList<>(asList(new Student(1,"Renan"), new Student(2,"Ricardo")));
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
